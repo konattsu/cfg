9c4669d81a47f8ebdffcac70b6147b9129214198
konattsu <139730998+konattsu@users.noreply.github.com>
2026-04-29 20:42:37 +0900

    Add installation script for Zsh and Oh My Posh
    Install Zsh, Oh My Zsh, and related plugins with necessary tools.

---
+++ zsh/install.sh
@@ -0,0 +1,34 @@
+# 1. apt 更新
+apt-get update
+apt-get upgrade -y
+
+# 2. 必要ツール
+apt-get install -y zsh git curl unzip fontconfig
+
+# 3. zsh をデフォルトシェルにする
+chsh -s "$(which zsh)"
+
+# 4. Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+zsh
+
+# 5. zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# 6. Oh My Posh
+curl -s https://ohmyposh.dev/install.sh | bash -s
+
+# PATH 反映
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.zshrc
+export PATH="$HOME/.local/bin:$PATH"
+
+# 7. themes
+mkdir -p ~/.poshthemes
+curl -fsSL https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -o /tmp/themes.zip
+unzip -o /tmp/themes.zip -d ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.omp.json
