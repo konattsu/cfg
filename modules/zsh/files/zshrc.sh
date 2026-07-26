e0e56c6b315739938ad24bf8e7c70c076adef3fe
konattsu <139730998+konattsu@users.noreply.github.com>
2026-07-27 02:25:39 +0900

    update oh-my-posh template and zsh highlighting

--- modules/zsh/files/zshrc.sh
+++ modules/zsh/files/zshrc.sh
@@ -14,6 +14,26 @@ plugins=(
 
 source $ZSH/oh-my-zsh.sh
 
+ZSH_HIGHLIGHT_STYLES[precommand]='fg=#FFE673,bold,underline'
+
+ZSH_HIGHLIGHT_STYLES[command]='fg=#4BB3CE'
+ZSH_HIGHLIGHT_STYLES[builtin]='fg=#4BB3CE'
+ZSH_HIGHLIGHT_STYLES[alias]='fg=#4BB3CE'
+ZSH_HIGHLIGHT_STYLES[function]='fg=#4BB3CE'
+
+ZSH_HIGHLIGHT_STYLES[unknown-token]='fg=#D0669A'
+
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument-unclosed]='fg=#FFC6E2'
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument-unclosed]='fg=#FFC6E2'
+ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument-unclosed]='fg=#FFC6E2'
+
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]='fg=#D8E9FC'
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]='fg=#D8E9FC'
+ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]='fg=#D8E9FC'
+
+ZSH_HIGHLIGHT_STYLES[globbing]='fg=#92F3A4'
+ZSH_HIGHLIGHT_STYLES[redirection]='fg=#92F3A4'
+
 # --- Prompt (oh-my-posh) ---
 export PATH="$HOME/.local/bin:$PATH"
 eval "$(oh-my-posh init zsh --config ~/.poshthemes/my_theme.omp.json)"
