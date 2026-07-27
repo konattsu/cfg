21a554f27d56d03595a7d55beba8138630215af7
konattsu <139730998+konattsu@users.noreply.github.com>
2026-07-27 11:02:10 +0900

    chores: fix keychain and improve oh-my-posh theme

--- modules/keychain/files/keychain.sh
+++ modules/keychain/files/keychain.sh
@@ -2,7 +2,5 @@
 moi_keychain_local="$HOME/.config/moi/keychain.local.sh"
 if [[ -r "$moi_keychain_local" ]]; then
   source "$moi_keychain_local"
-else
-  eval "$(keychain --eval)"
 fi
 unset moi_keychain_local
