01373be2e628757f3de2c19b750859182ea397a2
konattsu <139730998+konattsu@users.noreply.github.com>
2026-08-02 00:17:36 +0900

    nvim: markdownで補完切った

--- modules/nvim/files/lua/plugins/lang/markdown.lua
+++ modules/nvim/files/lua/plugins/lang/markdown.lua
@@ -5,6 +5,16 @@ return {
     ---@type AstroCoreOpts
     opts = {
       treesitter = { ensure_installed = { "markdown", "markdown_inline" } },
+      autocmds = {
+        markdown_disable_completion = {
+          {
+            event = "FileType",
+            pattern = "markdown",
+            desc = "Disable completion in Markdown buffers",
+            callback = function(args) vim.b[args.buf].completion = false end,
+          },
+        },
+      },
     },
   },
   {
